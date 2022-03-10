caa25c026c82aedf7d8cf5dae5841d7c34e500c7
JamesLiu <906028518@qq.com>
2022-03-11 01:33:58 +0800

    finish config reload test

--- src/main/scala/sample/gdmexchange/ClusterScheduler.scala
+++ src/main/scala/sample/gdmexchange/ClusterScheduler.scala
@@ -6,27 +6,27 @@ import sample.gdmexchange.DistributedConfig.ConfigItem
 
 object ClusterScheduler {
   sealed trait Task
-  case object ReloadConfigTask extends Task
+  case object ReloadConfigFromDBTask extends Task
   case object ReloadVaultTask extends Task
 
   def apply(distributedConfig: ActorRef[DistributedConfig.Command]) = {
     Behaviors.receiveMessage[Task] {
-      case ReloadConfigTask =>
+      case ReloadConfigFromDBTask =>
         //load from  db
         distributedConfig ! DistributedConfig.AddConfig(
           ConfigItem("key1", Some("value1"))
         )
         distributedConfig ! DistributedConfig.AddConfig(
-          ConfigItem("key2", Some("value1"))
+          ConfigItem("key2", Some("value2"))
         )
         Behaviors.same
       case ReloadVaultTask =>
         //load from vault
         distributedConfig ! DistributedConfig.AddConfig(
-          ConfigItem("key3", None, Some(1))
+          ConfigItem("key3", None, Some(3))
         )
         distributedConfig ! DistributedConfig.AddConfig(
-          ConfigItem("key4", None, Some(2))
+          ConfigItem("key4", None, Some(4))
         )
         Behaviors.same
     }
