8930b107c984f68a569ca8ddae18b9fc444dcc50
Chenyu.Liu <chenyu.liu@ascendex.io>
2022-03-11 13:05:41 +0800

    ClusterSingleton for scheduler

--- src/main/scala/sample/gdmexchange/UniversalModule.scala
+++ src/main/scala/sample/gdmexchange/UniversalModule.scala
@@ -1,29 +1,41 @@
 package sample.gdmexchange
 
-import akka.actor.typed.ActorRef
-import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.{ActorRef, SupervisorStrategy}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.cluster.typed.{ClusterSingleton, SingletonActor}
 import com.google.inject.{AbstractModule, Provides, Singleton}
 import com.typesafe.config.Config
 import io.gdmexchange.common.util.{Loggable, UuidUtil}
 import net.codingwell.scalaguice.ScalaModule
 
-case class UniversalModule(config:Config,actorContext: ActorContext[_]) extends AbstractModule with ScalaModule with Loggable{
+case class UniversalModule(config: Config, actorContext: ActorContext[_])
+    extends AbstractModule
+    with ScalaModule
+    with Loggable {
   override def configure(): Unit = {
     bind[ActorContext[_]].toInstance(actorContext)
     bind[Config].toInstance(config)
   }
 
-
   @Provides
   @Singleton
-  def distributedConfig:ActorRef[DistributedConfig.Command] = {
-    val actorName = "ddata-" + UuidUtil.newId(4)
-    val actorRef = actorContext.spawn(DistributedConfig("fp-api-server"), actorName)
-    logger.info("SPAWNING DISTRIBUED CONFIG ========== "+actorName)
+  def distributedConfig: ActorRef[DistributedConfig.Command] = {
+    val actorRef =
+      actorContext.spawn(DistributedConfig("fp-api-server"), "ddata")
+    logger.info("++++++++[Actor]:DistributedConfig spawned")
     actorRef
   }
 
   @Provides
   @Singleton
-  def clusterScheduler:ActorRef[ClusterScheduler.Task] = actorContext.spawn(ClusterScheduler(distributedConfig),"cluster-scheduler")
+  def clusterScheduler(
+      distributedConfig: ActorRef[DistributedConfig.Command]
+  ): ActorRef[ClusterScheduler.Task] = {
+    val singletonManager = ClusterSingleton(actorContext.system)
+    val actorRef: ActorRef[ClusterScheduler.Task] =
+      singletonManager.init(
+        SingletonActor(Behaviors.supervise(ClusterScheduler(distributedConfig)).onFailure[Exception](SupervisorStrategy.restart), "ClusterScheduler"))
+    logger.info("++++++++++[Actor]:ClusterScheduler spawned")
+    actorRef
+  }
 }
